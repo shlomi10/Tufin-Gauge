e53f260aa4c6ad927500574aec9e9a7e211298c3
Shlomi <shlomi10@gmail.com>
2024-01-18 14:56:31 +0200

    add exception

--- src/test/java/ApiImplementation.java
+++ src/test/java/ApiImplementation.java
@@ -9,7 +9,7 @@ public class ApiImplementation {
     String baseUrl = "https://reqres.in/api/users/";
 
     @Step("validate <firstName> <lastName> in list")
-    public void validateTobiasFunkeInList(String firstName, String lastName) throws IOException {
+    public void validateTobiasFunkeInList(String firstName, String lastName) {
         int counter = 0;
         for (int i = 1; i <= 12; i++) {
             try {
@@ -18,8 +18,8 @@ public class ApiImplementation {
                 if (actualFirstName.equals(firstName) && actualLastName.equals(lastName)) {
                     counter++;
                 }
-            } catch (Exception e){
-                System.out.println("There is no user in that page, the exception: "+e.getMessage());
+            } catch (Exception e) {
+                System.out.println("There is no user in that page, the exception: " + e.getMessage());
             }
         }
         System.out.println("Tobias was on the list " + counter + " time");
